4821f48730c3a86c3e0546bd69206fa038c04854
Jimbotz <jaymesaidbarbosahernandez@gmail.com>
2022-12-28 12:03:08 -0600

    Updates

---
+++ precache-manifest.0565bc5c197045efa246a97434b2dcac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32edaf5c991c235e32e",
+    "url": "/jimbotz.github.io/bia/static/css/main.ad0488b0.chunk.css"
+  },
+  {
+    "revision": "c32edaf5c991c235e32e",
+    "url": "/jimbotz.github.io/bia/static/js/main.c32edaf5.chunk.js"
+  },
+  {
+    "revision": "cf042e081f8a87233510",
+    "url": "/jimbotz.github.io/bia/static/js/1.cf042e08.chunk.js"
+  },
+  {
+    "revision": "8f70f951a2e026a2216d",
+    "url": "/jimbotz.github.io/bia/static/js/2.8f70f951.chunk.js"
+  },
+  {
+    "revision": "e09e02f87af314d4b814",
+    "url": "/jimbotz.github.io/bia/static/js/runtime~main.e09e02f8.js"
+  },
+  {
+    "revision": "d8db75d22c91f598402937eacbbf91b2",
+    "url": "/jimbotz.github.io/bia/static/media/bris.d8db75d2.png"
+  },
+  {
+    "revision": "733d332060742152f806c119df56f0ca",
+    "url": "/jimbotz.github.io/bia/static/media/feli.733d3320.png"
+  },
+  {
+    "revision": "3fb6f791164123776fe3c20b728af423",
+    "url": "/jimbotz.github.io/bia/static/media/quac.3fb6f791.jpg"
+  },
+  {
+    "revision": "3800fcb7b157538fa767fa537ca80880",
+    "url": "/jimbotz.github.io/bia/index.html"
+  }
+];
